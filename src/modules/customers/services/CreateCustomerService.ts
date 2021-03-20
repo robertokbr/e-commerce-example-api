aa85d39e54dfa36292fecf4b20f27bb6a7ddaa7a
robertokbr <robertojuniordev@gmail.com>
2021-03-20 15:16:48 -0300

    Add CreateCustomerService

--- src/modules/customers/services/CreateCustomerService.ts
+++ src/modules/customers/services/CreateCustomerService.ts
@@ -12,10 +12,26 @@ interface IRequest {
 
 @injectable()
 class CreateCustomerService {
-  constructor(private customersRepository: ICustomersRepository) {}
+  constructor(
+    @inject('CustomersRepository')
+    private customersRepository: ICustomersRepository,
+  ) {}
 
   public async execute({ name, email }: IRequest): Promise<Customer> {
-    // TODO
+    const customerWithSameEmail = await this.customersRepository.findByEmail(
+      email,
+    );
+
+    if (customerWithSameEmail) {
+      throw new AppError('Provided email is already in use');
+    }
+
+    const customer = await this.customersRepository.create({
+      email,
+      name,
+    });
+
+    return customer;
   }
 }
 
